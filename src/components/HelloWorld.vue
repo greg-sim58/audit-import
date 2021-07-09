8d097f68ba3cb3d644b3cf51fb4abd6732957fbb
greg-sim58 <greg@sim58dev.com>
2021-07-09 12:39:34 +0200

    Added StartDate, EndDate, Year

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -35,7 +35,6 @@ export default {
       audits: audits
     }),
     created() {
-      // console.log('AUDITS: ', this.audits)
       db.info()
     },
 
@@ -44,20 +43,14 @@ export default {
         let xlsxfile = event.target.files ? event.target.files[0] : null;
         var cnt = 0
         readXlsxFile(xlsxfile).then((rows) => {
-          // console.log("rows:", rows)
+          const year = rows[0][2]
           var records = []
           rows.forEach(element => {
-            console.log('CNT: ', cnt)
-            // console.log('DATA: ', element)
-            var record = { Item: element[0],  AuditPerQtr: element[1], Month: element[2], Week: element[3], Date: element[4], Municipality: element[5], RegisteringAuthority: element[6], Days: element[7], AuditType: element[8], AuditOfficer: element[9]}
-            // console.log(record)
+            var record = { Item: element[0],  AuditPerQtr: element[1], Year: this.startYear(element), Month: element[2], Week: element[3], Date: element[4], StartDate: this.startDate(element), EndDate: this.endDate(element), Municipality: element[5], RegisteringAuthority: element[6], Days: element[7], AuditType: element[8], AuditOfficer: element[9]}
             records.push(record)
-            // element.forEach(item => {
-            //   console.log(item)
-            // })
             cnt++
           })
-          console.log(records)
+          console.log(records[0])
           db.bulkDocs(records)
           .then(response => {
             console.info(response)
@@ -66,6 +59,32 @@ export default {
             console.log(' ERROR: ', err)
           })
         })
+      },
+      startDate(auditItem) {
+        if (auditItem[4] !== null) {
+          return auditItem[4].split(/(\-+)/).filter( function(e) { return e.trim().length > 0; } )[0] + ' ' + auditItem[2]
+        } else {
+          return ''
+        }
+        
+      },
+      endDate(auditItem) {
+        const date = auditItem[4]
+        if (auditItem[4] !== null) {
+          const splitted = auditItem[4].split(/(\-+)/).filter( function(e) { return e.trim().length > 0; } )// [0];
+          const splitted2 = splitted[2].split(/(\s+)/).filter( function(e) { return e.trim().length > 0; } );
+          var d1 = splitted
+        return splitted2[0] + ' ' + auditItem[2]
+        } else {
+          return ''
+        }
+      },
+      startYear(auditItem) {
+        if (auditItem[2] !== null) {
+          return auditItem[2].split(/(\s+)/).filter( function(e) { return e.trim().length > 0; } )[1];          
+        } else {
+          return ''
+        }
       }
     }
   }
